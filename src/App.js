d05f8ed87ef44692acb71cf943962dea2f649f03
Louis-Amand <48965957+Louis-Amand@users.noreply.github.com>
2023-05-04 10:44:04 +0200

    adding test back message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [testMessage, setTestMessage] = useState('');
+
+  useEffect(() => {
+    const fetchTestMessage = async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/api/test');
+        setTestMessage(response.data);
+      } catch (error) {
+        console.error('Erreur lors de la récupération du message de test:', error);
+      }
+    };
+
+    fetchTestMessage();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Front-end React</h1>
+      <p>Message reçu du back-end Java Spring: {testMessage}</p>
     </div>
   );
 }
