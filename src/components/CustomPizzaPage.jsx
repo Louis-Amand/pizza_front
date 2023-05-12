639ab7d02c3ba5ae3dc0d8aa36fa271c376dcc10
Louis-Amand <48965957+Louis-Amand@users.noreply.github.com>
2023-05-13 00:21:47 +0200

    ajout du prix total

--- src/components/CustomPizzaPage.jsx
+++ src/components/CustomPizzaPage.jsx
@@ -1,14 +1,15 @@
 import * as React from 'react';
 import Pizza from '../pizza.svg';
-import {FormPizza} from "./public/FormPizza";
+import { FormPizza } from "./public/FormPizza";
 
 export function CustomPizzaPage() {
     return (
         <>
             <h2 className={"title-generazza"}>Je choisis mes ingredients pour une pizza personnel !</h2>
+            <h3 className={"title-generazza-2"}>Commencer votre pizza avec une base à 11 €</h3>
             <div className={"customFormDisplay"}>
-                <FormPizza/>
-                <img width={"500px"} src={Pizza} alt=""/>
+                <FormPizza />
+                <img className='img-size' src={Pizza} alt="" />
             </div>
         </>
     );
