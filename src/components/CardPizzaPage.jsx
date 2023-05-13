8c30b0d1893f7c5e2acecdcc053cf667d28644ef
Louis-Amand <48965957+Louis-Amand@users.noreply.github.com>
2023-05-13 02:48:50 +0200

    ajout de required, nouvelle archi pour les donnees transmises

--- src/components/CardPizzaPage.jsx
+++ src/components/CardPizzaPage.jsx
@@ -19,7 +19,6 @@ const CardPizzaPage = () => {
     }, []);
 
     const handleOrder = (pizzaId) => {
-        // Passer les informations nécessaires pour la commande à la page correspondante
         const pizzaData = {
             id: pizzaId,
             type: pizzas.find((pizza) => pizza.id === pizzaId).name,
--- src/components/CommandPage.jsx
+++ src/components/CommandPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
 
 const CommandPage = () => {
     const [formState, setFormState] = useState({
@@ -29,10 +30,57 @@ const CommandPage = () => {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log(formState);
-        axios.post('/api/commandes', formState)
+
+        // Vérifier si les champs du formulaire sont vides
+        if (
+            formState.name.trim() === '' ||
+            formState.firstName.trim() === '' ||
+            formState.phone.trim() === '' ||
+            formState.address.trim() === '' ||
+            formState.cardNumber.trim() === '' ||
+            formState.expiryDate.trim() === '' ||
+            formState.cvv.trim() === ''
+        ) {
+            // Gérer le cas où un champ est vide
+            console.error('Veuillez remplir tous les champs du formulaire.');
+            return;
+        }
+
+        const orderId = uuidv4(); // Générer un UUID pour la commande
+        const isCustom = formState.pizza.ingredients && formState.pizza.base;
+        let pizzaData = {};
+
+        if (isCustom) {
+            pizzaData = {
+                pizzaname: 'Générazza',
+                price: formState.pizza.price,
+                base: formState.pizza.base,
+                ingredients: formState.pizza.ingredients
+            };
+        } else {
+            pizzaData = {
+                pizzaname: formState.pizza.type,
+                price: formState.pizza.price,
+            };
+        }
+
+        const orderData = {
+            orderId: orderId,
+            hour: new Date().toISOString(),
+            name: formState.name,
+            firstName: formState.firstName,
+            phone: formState.phone,
+            address: formState.address,
+            price: formState.pizza.price,
+            pizza_name: pizzaData.pizzaname,
+            ...(formState.pizza.base && { pizza_base: pizzaData.base }),
+            ...(formState.pizza.ingredients && { pizza_ingredients: pizzaData.ingredients })
+        };
+
+        axios
+            .post('http://localhost:8080/api/commandes', orderData)
             .then(response => {
-                console.log(response);
+                window.localStorage.setItem(orderId, JSON.stringify(orderId));
                 // Rediriger l'utilisateur vers une autre page ou afficher un message de succès
             })
             .catch(error => {
@@ -40,6 +88,7 @@ const CommandPage = () => {
                 // Gérer l'erreur (par exemple, afficher un message d'erreur à l'utilisateur)
             });
     };
+
 
     return (
         <div>
@@ -53,34 +102,35 @@ const CommandPage = () => {
             <form onSubmit={handleSubmit}>
                 <label>
                     Nom :
-                    <input type="text" name="name" onChange={handleChange} />
+                    <input type="text" name="name" onChange={handleChange} required />
                 </label>
                 <label>
                     Prénom :
-                    <input type="text" name="firstName" onChange={handleChange} />
+                    <input type="text" name="firstName" onChange={handleChange} required />
                 </label>
                 <label>
                     Téléphone :
-                    <input type="text" name="phone" onChange={handleChange} />
+                    <input type="text" name="phone" onChange={handleChange} required />
                 </label>
                 <label>
                     Adresse :
-                    <input type="text" name="address" onChange={handleChange} />
+                    <input type="text" name="address" onChange={handleChange} required />
                 </label>
 
                 <h2 className='page-title-2'>Informations bancaires :</h2>
                 <label>
                     Numéro de la carte :
-                    <input type="text" name="cardNumber" onChange={handleChange} />
+                    <input type="text" name="cardNumber" onChange={handleChange} required />
                 </label>
                 <label>
                     Date d'expiration :
-                    <input type="text" name="expiryDate" onChange={handleChange} placeholder="MM/YY" />
+                    <input type="text" name="expiryDate" onChange={handleChange} placeholder="MM/YY" required />
                 </label>
                 <label>
                     CVV :
-                    <input type="text" name="cvv" onChange={handleChange} />
+                    <input type="text" name="cvv" onChange={handleChange} required />
                 </label>
+
 
                 <input type="submit" value="Commander" />
             </form>
